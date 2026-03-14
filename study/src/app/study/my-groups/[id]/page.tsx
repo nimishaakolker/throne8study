95e8df93ad125878f3ea9c8123f6db9a39076345
Nimisha Akolker <nimishaakolker@gmail.com>
2026-03-14 16:47:35 +0530

    fixed chat page not found issue

--- study/src/app/study/my-groups/[id]/page.tsx
+++ study/src/app/study/my-groups/[id]/page.tsx
@@ -5,6 +5,5 @@ import GroupRoom from '../components/GroupRoom';
 
 export default function GroupRoomPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = use(params);
-  
   return <GroupRoom groupId={parseInt(id)} />;
 }
